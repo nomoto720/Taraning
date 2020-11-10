03ec4b26b04fb21ac0d86ae3e50efbb118fd6bd8
nomoto <tka303@gmail.com>
2020-11-10 09:42:02 +0900

    ArrLesson3修正

--- java/20201109/ArrLesson3.java
+++ java/20201109/ArrLesson3.java
@@ -3,12 +3,23 @@ public class ArrLesson3{
 	public static void main(String[] args){	
 	int[] dataA={3,5,10};
 	int[] dataB=new int[]{4,6,10};
+	String[] strs={"hoge","fuga","bar"};
+	double[] nums={1.1,2.2,3.3};
+	boolean[] bools={false,false,true,true,};
 	for(int i =0;i<dataA.length; i++){
 		System.out.println(dataA[i]);
 	}
 	for(int n: dataB){
 		System.out.println(n);
 	}
+	for(int j =0;j<strs.length;j++){
+		System.out.println(strs[j]);
+	}
+	for(String s : strs){
+		System.out.println(s);
+	}
+	
+
 	System.out.println(Arrays.toString(dataA));
 	}
 }
