b8ef061de590626e0017da81895a53ae4be02439
nomoto <tka303@gmail.com>
2020-11-11 14:35:54 +0900

    Arr5.java,Arr6.java追加

--- java/20201111/Arr4.java
+++ java/20201111/Arr4.java
@@ -2,15 +2,13 @@ import java.util.*;
 public class Arr4{
 	public static void main(String[] args){
 		int[] nums=new int[10];	
+		String[] strs=new String[10];
 		for(int i=0;i<nums.length;i++){
 			nums[i]=new Random().nextInt(nums.length);
 			System.out.print(nums[i]+",");
-		}
-		String[] parity=new String[10];
-		for(int i=0;i<nums.length;i++){
-			parity[i]=nums[i]%2==0?"偶数":"奇数";
+			strs[i]=nums[i]%2==0?"偶数":"奇数";
 		}
 		System.out.println();
-		System.out.println(Arrays.toString(parity));
+		System.out.println(Arrays.toString(strs));
 	}
 }
