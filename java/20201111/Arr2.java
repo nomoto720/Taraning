e17246af97da0490a52bd5b109fa3b9f65d5d22e
nomoto <tka303@gmail.com>
2020-11-11 10:29:16 +0900

    Arr2.java変更

--- java/20201111/Arr2.java
+++ java/20201111/Arr2.java
@@ -5,13 +5,17 @@ public class Arr2{
 		int[] arr2={3,9,2,5};
 		int[] arr3=new int[4];
 		for(int i=0;i<arr3.length;i++){
+		/*
 			if(arr1[i]>arr2[i]){
 				arr3[i]=arr1[i];
 			}else{
 				arr3[i]=arr2[i];
 			}
+			*/
+			arr3[i]=arr1[i]>arr2[i]?arr1[i]:arr2[i];
 		}
-		System.out.println(Arrays.toString(arr3));
+		System.out.println(Arrays.toString(arr3));	
+		
 	}
 }
 
