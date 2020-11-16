6394a25b5c31e56972b943af0eb8abd52fde5131
nomoto <tka303@gmail.com>
2020-11-16 14:34:43 +0900

    テスト対策txtファイル追加

--- java/20201116/ArrLesson5.java
+++ java/20201116/ArrLesson5.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class ArrLesson5{
 	public static void main(String[] args){
 		System.out.println("--掛け算表--!");
-		System.out.print("n(1~9)?>");
+		System.out.print("n(1~1000)?>");
 		int num=new Scanner(System.in).nextInt();
 		int[][] nums=new int[num][9];
 		for(int i=0;i<nums.length;i++){
@@ -12,7 +12,7 @@ public class ArrLesson5{
 		}
 		for(int i=0;i<nums.length;i++){
 			for(int j=0;j<nums[i].length;j++){
-				System.out.printf("%3d|",nums[i][j]);
+				System.out.printf("%5d|",nums[i][j]);
 			}
 			System.out.println();
 		}
