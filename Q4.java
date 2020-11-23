d073f31ad3c97e36e7140eb83d690cfc6274d1b7
nomoto <tka303@gmail.com>
2020-11-24 07:39:57 +0900

    Q4.java追加

--- Q4.java
+++ Q4.java
@@ -5,17 +5,33 @@ public class Q4{
 			String name=new Scanner(System.in).nextLine();
 			while(true){
 				System.out.println("なにがしたいですか?");
-				System.out.print("0...文字数を確認する 1...姓名占いをする2...終了する>");
+				System.out.printf("0...文字数を確認する%n1...占いをする%n2...終了する>%n");
 				int doing=new Scanner(System.in).nextInt();
 				if(doing==0){
 					System.out.printf("%sさんの文字数は%sです。%n",name,nameCount(name));
 				}else if(doing==1){
+					System.out.printf("%sさんの今日の運勢は%sです。%n",name,nameFortune());
 				}else{
 					System.out.println("終了します。");
 					break;
 				}
 		}
 	}
-	static String nameCount(String name);
-		return name.lenght();
+	static int nameCount(String name){
+		return name.length();
+	}
+	static String nameFortune(){
+		int luckyNum=new Random().nextInt(4)+1;
+		switch(luckyNum){
+			case 1:
+			return "大吉";
+			case 2:
+			return "吉";
+			case 3:
+			return "小吉";
+			case 4:
+			return "凶";
+		}
+		return "吉" ;
+	}	
 }
