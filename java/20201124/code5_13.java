e7f56655e3e2f24d2412791aece78bfe9740cf85
nomoto <tka303@gmail.com>
2020-11-24 09:56:16 +0900

    Code5_14追加

--- java/20201124/code5_13.java
+++ java/20201124/code5_13.java
@@ -1,8 +1,16 @@
 public class Code5_13{
 	public static void incArray(int[] array){
+		int[] temp=new int[array.length];
+		for(int i=0;i>temp.length;i++){
+			temp[i]=array[i];
+		}
 		for(int i=0;i<array.length;i++){
 			array[i]++;
 		}
+	}
+	public static void incNum(int n){
+		n++;
+		System.out.println(n);
 	}
 	public static void main(String[] args){
 		int[] array={1,2,3};
@@ -10,5 +18,8 @@ public class Code5_13{
 		for(int i:array){
 			System.out.println(i);
 		}
+		int n=10;
+		incNum(n);
+		System.out.println(n);
 	}
 }
