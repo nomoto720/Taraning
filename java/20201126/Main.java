fd76a9a5e46d4212a137ad5febc437c563f0820f
nomoto <tka303@gmail.com>
2020-11-26 14:23:09 +0900

    Main.java追加

---
+++ java/20201126/Main.java
@@ -0,0 +1,30 @@
+import my.util.*;
+import java.util.*;
+public class Main{
+	public static void main(String[] args){
+		System.out.print("配列の要素数を入力してください>");
+		int count=new Scanner(System.in).nextInt();
+		int min=-10;int max=10;
+		int[] arrRandom = Common.arrRandomRange(min,max,count);
+		System.out.println("配列を生成しました。");
+		System.out.println(Arrays.toString(arrRandom));
+		int[] arr=Common.arrClone(arrRandom);
+		while(true){
+		System.out.print("1...元の配列を表示,2...配列を昇順ソート,3...配列を降順ソート,4...終了>");
+		int num=new Scanner(System.in).nextInt();
+			if(num==1){
+				System.out.println(Arrays.toString(arr));
+			}else if(num==2){
+				Common.arrSort(arrRandom);
+				System.out.println(Arrays.toString(arrRandom));
+			}else if(num==3){
+				Common.arrSort(arrRandom,true);
+				System.out.println(Arrays.toString(arrRandom));
+			}else{
+				System.out.println("アプリケーションを終了します");
+				break;
+			}
+
+		}
+	}
+}
