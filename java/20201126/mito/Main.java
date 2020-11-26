dfec2962746944e33fc456b2c539758255ae63a7
nomoto <tka303@gmail.com>
2020-11-26 11:06:06 +0900

    ファイル修正

---
+++ java/20201126/mito/Main.java
@@ -0,0 +1,10 @@
+import comment.Zenhan;
+import comment.Kouhan;
+public class Main{
+	public static void main(String[] args) throws Exception{
+		Zenhan.doWarusa();
+		Zenhan.doTogame();
+		Kouhan.callDeae();
+		Kouhan.showMondokoro();
+	}
+}
