7dad03ec629851e1de1abbaab4c14f66d74f6753
nomoto <tka303@gmail.com>
2020-11-27 09:40:16 +0900

    Main.java修正

--- java/20201127/rpg/Main.java
+++ java/20201127/rpg/Main.java
@@ -1,4 +1,11 @@
 public class Main{
 	public static void main(String[] args){
+		Hero h=new Hero();
+		h.name="ロト";
+		h.sleep();
+		System.out.println(h.hp);
+		Hero h2=new Hero();
+		h2.name="ジェシカ";
+		h2.sleep();
 	}
 }
