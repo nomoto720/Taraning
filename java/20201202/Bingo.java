07b4f7e51f184181b5fc111dbde15fe11727fc56
nomoto <tka303@gmail.com>
2020-12-02 11:35:25 +0900

    Bingo.java修正

--- java/20201202/Bingo.java
+++ java/20201202/Bingo.java
@@ -5,19 +5,31 @@ public class Bingo{
 		System.out.print("幅>");
 		int width=scan.nextInt();
 		int bingoCard[][]=makeCard(width);
-		for(int i=0;i<bingoCard.length;i++){
-			for(int j=0;j<bingoCard.length;j++){
-			System.out.print(bingoCard[i][j]);
+		/*for(int[] line: bingoCard){
+			System.out.println(Arrays.toString(line));
+		}
+		*/
+		for(int[] line: bingoCard){
+			for(int n:line){
+				System.out.print(n);
 			}
 			System.out.println();
 		}
+
+		/*for(int i=0;i<bingoCard.length;i++){
+			for(int j=0;j<bingoCard.length;j++){
+				System.out.print(bingoCard[i][j]);
+			}
+			System.out.println();
+		}
+		*/
 	}
 	static int[][] makeCard(int width){
 		Random ram=new Random();
 		int[][] card=new int[width][width];
 		for(int i=0;i<card.length;i++){
 				for(int j=0;j<card.length;j++){
-						card[i][j]=ram.nextInt(8)+1;
+						card[i][j]=ram.nextInt(9)+1;
 				}
 		}
 		return card;
