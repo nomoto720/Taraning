6cbcff35512ff4f08148eb9d5c9bf9ac778834c2
nomoto <tka303@gmail.com>
2020-12-02 12:22:17 +0900

    追加

--- java/20201202/Bingo.java
+++ java/20201202/Bingo.java
@@ -5,24 +5,8 @@ public class Bingo{
 		System.out.print("幅>");
 		int width=scan.nextInt();
 		int bingoCard[][]=makeCard(width);
-		/*for(int[] line: bingoCard){
-			System.out.println(Arrays.toString(line));
-		}
-		*/
-		for(int[] line: bingoCard){
-			for(int n:line){
-				System.out.print(n);
-			}
-			System.out.println();
-		}
-
-		/*for(int i=0;i<bingoCard.length;i++){
-			for(int j=0;j<bingoCard.length;j++){
-				System.out.print(bingoCard[i][j]);
-			}
-			System.out.println();
-		}
-		*/
+		showCard(bingoCard);
+		boolean a=isSame(bingCard[0][0]);
 	}
 	static int[][] makeCard(int width){
 		Random ram=new Random();
@@ -33,5 +17,44 @@ public class Bingo{
 				}
 		}
 		return card;
+	}
+		static void showCard(int[][] card){
+		for(int i=0;i<card.length;i++){
+			for(int j=0;j<card.length;j++){
+				System.out.print(card[i][j]);
+			}
+			System.out.println();
+		}
+
+		/*for(int[] line: bingoCard){
+			System.out.println(Arrays.toString(line));
+		}*/
+
+		/*for(int[] line: bingoCard){
+			for(int n:line){
+				System.out.print(n);
+			}
+			System.out.println();
+		}*/
+	}
+	static boolean isSame(int[] line){
+			boolean isSame=true;
+			int first=line[0];
+			if(int i=1 ; i<line.length ; i++){
+				if(first !=line[i]){
+				isSame=false;
+				break;
+				}
+			}
+			return isSame;
+	}
+	static int[][] horizontalLines(int[][] card){
+		int[][] lines=new int[card.length][card.length];
+		for(int i=0;i<card.length;i++){
+			for(int j=0;j<card.length;j++){
+				lines[i][j]=card[i][j];
+			}
+		}
+		return lines;
 	}
 }
