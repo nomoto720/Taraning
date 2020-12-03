2dc93d3a630e0ecc15505c1990c7c88fcc6de793
nomoto <tka303@gmail.com>
2020-12-03 10:09:53 +0900

    修正

--- java/20201203/Overload.java
+++ java/20201203/Overload.java
@@ -1,9 +1,11 @@
 import java.util.*;
 public class Overload{
 	public static void main(String[] args){
+		int[] nums1=createRandomArr(1,9,3);
+		showArr(nums1);
 		int[][] nums=createRandomArr(1,9,3,2);
-		for(int i=0;i<nums.length;i++){
-			System.out.println(Arrays.toString(nums[i]));
+		for(int[] arr:nums ){
+			System.out.println(Arrays.toString(arr));
 		}	
 	}
 	static int[] createRandomArr(int min,int max,int len){
@@ -20,5 +22,12 @@ public class Overload{
 			nums[i]=createRandomArr(min,max,childlen);
 		}
 		return nums;
+	}
+	static void showArr(int[] arr){
+		System.out.print("{");
+		for(int n:arr){
+			System.out.print(n+",");
+		}
+		System.out.println("}");
 	}
 }
