4f69c1285f062235733e154ce490f1847604e986
nomoto <tka303@gmail.com>
2020-12-03 10:40:47 +0900

    SplitLesson.java

--- java/20201203/Overload.java
+++ java/20201203/Overload.java
@@ -34,6 +34,7 @@ public class Overload{
 	static void showArr(int[][] arr){
 		System.out.println("{");
 		for(int i=0;i<arr.length;i++){
+			System.out.print(" ");
 			showArr(arr[i]);
 		}
 		System.out.println("}");
---
+++ java/20201203/SplitLesson.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class SplitLesson{
+	public static void main(String[] args){
+		String str="りんご,みかん,ばなな";
+		String[] data=str.split(",");
+		System.out.println(Arrays.toString(data));
+	}
+}
