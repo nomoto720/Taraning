b2af24ad259a092f2494ec1c8b08143b5759514a
nomoto <tka303@gmail.com>
2020-12-03 14:34:39 +0900

    追加

--- java/20201203/Pool.java
+++ java/20201203/Pool.java
@@ -5,6 +5,10 @@ public class Pool{
 			this.water=0.0;
 		}
 		public void feedWater(double water){
+			if(this.water==MAX){
+				System.out.println("満水です");
+				return;
+			}
 			double feed= water+this.water > this.MAX ? this.MAX-this.water : water;  
 			this.water+=feed;
 			System.out.printf("%.1f立方メートル給水しました。%n",feed);
