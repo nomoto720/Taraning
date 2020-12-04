5a93ae73e0d49dbe1c40db6b67279ceea0f808cf
nomoto <tka303@gmail.com>
2020-12-04 09:36:26 +0900

    修正

--- java/20201204/rpg/Main.java
+++ java/20201204/rpg/Main.java
@@ -1,6 +1,7 @@
 public class Main{
 	public static void main(String[] args){
-		SuperHero sh=new SuperHero();
-		sh.run();
+		//SuperHero sh=new SuperHero();
+		//sh.run();
+		Weapon w=new Weapon();
 	}
 }
