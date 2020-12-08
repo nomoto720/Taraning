d30f885be9b7f632478b38ff33aaeee4a7aaca4b
nomoto <tka303@gmail.com>
2020-12-08 13:08:09 +0900

    ArrayListLesson.java修正

--- java/20201208/ArrayListLesson.java
+++ java/20201208/ArrayListLesson.java
@@ -2,5 +2,20 @@ import java.util.*;
 public class ArrayListLesson{
 	public static void main(String[] args){
 		ArrayList<String> names=new ArrayList<>();
+		names.add("Jhon");
+		names.add("Paul");
+		names.add("George");
+		
+		System.out.println(names.get(0));//John
+		System.out.println(names.size());//3
+
+		for(int i=0;i<names.size();i++){
+			System.out.println(names.get(i));
+		}
+		for(String s:names){
+			System.out.println(s);
+		}
+		names.remove(0);
+		System.out.println(names.size());
 	}
 }
