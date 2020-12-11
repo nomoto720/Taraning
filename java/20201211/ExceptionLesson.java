afb57dc15830be5abd75137aa37bd6395fd28dcb
nomoto <tka303@gmail.com>
2020-12-11 13:35:31 +0900

    追加

--- java/20201211/ExceptionLesson.java
+++ java/20201211/ExceptionLesson.java
@@ -1,12 +1,8 @@
 import java.util.*;
 public class ExceptionLesson{
-	public static void main(String[] args){
+	public static void main(String[] args) throws Exception{
 		System.out.println("Start");
-		try{
 			Thread.sleep(3000);
-		}catch(InterruptedException e){
-			System.out.println(e.getMessage());
-		}
 		System.out.println("End");
 	}
 }
