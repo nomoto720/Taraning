e8321030eeb80093834cbaf04cdde90ad2938d1d
nomoto <tka303@gmail.com>
2020-12-11 17:30:03 +0900

    kaike.java

--- java/20201211/Kaikei.java
+++ java/20201211/Kaikei.java
@@ -6,19 +6,41 @@ public class Kaikei{
 		InputStreamReader isr=new InputStreamReader(fis,"utf-8");
 		BufferedReader br=new BufferedReader(isr);
 		System.out.println("csvﾃﾞｰﾀ");
-		System.out.println("------");
+		System.out.println("——————");
 		String line;
-		Map<String,Map<String,Integer>>lines=new HashMap<>();
-		Map<String,Integer>list=new HashMap<>();
+		Map<String,Integer> personalAcct= new HashMap<>();
+		Map<String,Integer> campAcct=new HashMap<>();
 		while((line=br.readLine())!=null){
 			System.out.println(line);
-			string str=line;
-			string[] strs=str.split(",");
-			int money=Integer.valueOf(strs[2]);
+			String[] data=line.split(",");
+			int acct=Integer.valueOf(data[2]);
 
-			lines.put(strs[0],(stars,[1],money));
+			int persAcct=personalAcct.containsKey(data[0])?personalAcct.get(data[0])+acct:acct;
+			personalAcct.put(data[0],persAcct);
+
+			acct=campAcct.containsKey(data[1])?campAcct.get(data[1])+acct:acct;
+			campAcct.put(data[1],acct);
 		}
 		br.close();
+		System.out.println();
+		System.out.println("キャンプ会計");
+		System.out.println("——————");
+		int campTotal=0;
+		for(String key:campAcct.keySet()){
+			System.out.println(key+":"+campAcct.get(key));
+			campTotal+=campAcct.get(key);
+		}
+		System.out.println("——————");
+		System.out.println("合計:"+campTotal+"円");
+		System.out.println();
+		int acctPerPerson=campTotal/personalAcct.size();
+		System.out.println("個人別会計(一人あたり:"+acctPerPerson+"円)");
+		System.out.println("——————");
+		for(String key:personalAcct.keySet()){
+			int income=personalAcct.get(key)-acctPerPerson;
+			String str=income>0?"+"+income:""+income;
+			System.out.println(key+":"+str);
+		}
 	}
 }
 
