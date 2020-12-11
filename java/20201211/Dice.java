f823a4f0a970539c10b1ab828f02f8a4605fdee5
nomoto <tka303@gmail.com>
2020-12-11 10:29:27 +0900

    Dice.java追加

--- java/20201211/Dice.java
+++ java/20201211/Dice.java
@@ -6,9 +6,16 @@ public class Dice{
 		System.out.print("サイコロを何回ふりますか>");
 		int num=sc.nextInt();
 		Map<Integer,Integer> dice=new LinkedHashMap<>();
+		dice.put(1,0);
+		dice.put(2,0);
+		dice.put(3,0);
+		dice.put(4,0);
+		dice.put(5,0);
+		dice.put(6,0);
 		for(int i=0;i<num;i++){
 			int roll=rand.nextInt(6)+1;
-			dice.put(i,roll);
+			int count=dice.get(roll)+1;
+			dice.put(roll,count);	
 		}
 		for(Integer key:dice.keySet()){
 			System.out.println(key+"..."+dice.get(key)+"回");
