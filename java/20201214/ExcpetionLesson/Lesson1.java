1299fb3c5d9dc8e085e384888c2ac66f06e4fe42
nomoto <tka303@gmail.com>
2020-12-14 09:22:46 +0900

    Lesson1.java追加

--- java/20201214/ExcpetionLesson/Lesson1.java
+++ java/20201214/ExcpetionLesson/Lesson1.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 public class Lesson1{
 	public static void main(String[] args) throws Exception{
-		FileWriter fw =new FileWriter("data.txt");
+		FileWriter fw =new FileWriter("data.txt",true);
 		fw.write("hello!");
 		fw.close();
 	}
