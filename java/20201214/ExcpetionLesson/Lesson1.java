171e9249d61d16597910bc4cb8456abf53bbe22a
nomoto <tka303@gmail.com>
2020-12-14 09:17:14 +0900

    20201214追加

---
+++ java/20201214/ExcpetionLesson/Lesson1.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+public class Lesson1{
+	public static void main(String[] args) throws Exception{
+		FileWriter fw =new FileWriter("data.txt");
+		fw.write("hello!");
+		fw.close();
+	}
+}
