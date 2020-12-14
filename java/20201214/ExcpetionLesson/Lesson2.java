36b402e0ec99d4ec1551b9ca9e81e94610d092cc
nomoto <tka303@gmail.com>
2020-12-14 09:37:50 +0900

    修正

---
+++ java/20201214/ExcpetionLesson/Lesson2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class Lesson2{
+	public static void main(String[] args) {
+			FileWriter fw=null; 
+		try{
+			fw.write("hello!");
+			fw.close();
+		}catch(Exception e){
+			System.out.println("何らかの例外が発生しました");
+		}finally{	
+			try{
+			fw.close();
+			}catch(IOException e){
+				;
+			}
+		}
+	}
+}
