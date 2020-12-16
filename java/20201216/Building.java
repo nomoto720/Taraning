b528aec619abe52751f84d18f3dcf8cb1eb2c595
nomoto <tka303@gmail.com>
2020-12-16 11:00:14 +0900

    追加

--- java/20201216/Building.java
+++ java/20201216/Building.java
@@ -1,7 +1,12 @@
+import java.util.*;
 public class Building{
 	int width;
+	static int allWidth;
+	static int maxWidth=0;
 	public Building(int width){
 		this.width=width;
+		this.allWidth+=width;
+		this.maxWidth=maxWidth<width ? width : maxWidth;
 	}
 	public void display(){
 			for(int i=0;i<this.width;i++){
@@ -10,5 +15,20 @@ public class Building{
 				}
 				System.out.println();
 			}
+	}
+	public static void displayAll(List<Building> list){
+		for(int i=0;i<maxWidth;i++){
+				for(int j=0;j<list.size();j++){
+					for(int k=0;k<list.get(j).width;k++){
+						if(maxWidth-i>list.get(j).width){
+							System.out.print(" ");
+						}else{
+							System.out.print("#");
+						}
+					}
+					System.out.print(" ");
+				}
+				System.out.println();
+		}
 	}
 }
--- java/20201216/BuildingApp.java
+++ java/20201216/BuildingApp.java
@@ -1,10 +1,36 @@
 import java.util.*;
 public class BuildingApp{
 	public static void main(String[] args){
-		Scanner sc=new Scanner(System.in);
+		/*Scanner sc=new Scanner(System.in);
+		List<Building> list=new ArrayList<>();
+		for(int i=0;i<5;i++){
+			System.out.print("建物の幅>");
+			int width=sc.nextInt();
+			Building building=new Building(width);
+			list.add(building);
+			building.display();
+		}	
+		Building.displayAll(list); */
+
+		/*Scanner sc=new Scanner(System.in);
 		System.out.print("建物の幅>");
 		int width=sc.nextInt();
 		Building b1=new Building(width);
-		b1.display();
+		b1.display();*/
+
+		
+		Building b1=new Building(12);
+		Building b2=new Building(10);
+		Building b3=new Building(7);
+		Building b4=new Building(15);
+		Building b5=new Building(7);
+		List<Building> list=new ArrayList<>();
+		list.add(b1);
+		list.add(b2);
+		list.add(b3);
+		list.add(b4);
+		list.add(b5);
+		Building.displayAll(list);
+		
 	}
 }
